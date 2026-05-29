dab8e82860923fefba82a94372a33450b5f93f61
Torbiee <jack@phthalo.net>
2026-05-28 22:41:03 -0400

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,34 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+	<html class="bg-gray-700">
+	<div class="fixed inset-x-0 top-0 h-8 bg-slate-800 border-b border-slate-600">
+	  <div class="flex items-center justify-between gap-8 mt-1 px-4 sm:px-6">
+	     <div class="flex items-center gap-4">
+		<h1 class="font-serif text-white">Jack Dockus</h1>
+	     </div>
+		<div class="flex items-center gap-6">
+		  <a class="text-sm/6 text-white" href="">Link</a>
+		</div>
+	  </div>
+	</div>
+
+	  <div class="md:flex-initial items-center bg-gray-700 grid grid-cols-3 grid-flow-row auto-rows-max gap-y-10">
+
+	 	 <div class="flex-initial m-10 col-2 rounded-xl bg-indigo-950 px-10 py-4 shadow-lg outline-2 outline-offset-1 outline-indigo-900">
+		  <div>
+	    		<div class="text-xl font-medium text-pink-200">Home Page</div>
+	    		<p class="text-gray-500 dark:text-gray-400">I made random stuff!</p>
+		   </div>
+		</div>
+
+	<div class="mx-auto items-center bg-gray-700">
+		<button class="outline-1 outline-offset-0 outline-pink-300 bg-orange-300 
+		transition duration-700 ease-in-out hover:-translate-y-1 hover:scale-200 hover:bg-orange-600">Button</button>
+	</div>
+
+     </div>
+   </html>
   );
 }
+
